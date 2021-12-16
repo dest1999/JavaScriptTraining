cfe5e76b5bb9ec577e884a74f3af3446e8524fee
Oleg <serv_all@mail.ru>
2021-12-16 22:51:51 +0300

    DZ-1

---
+++ lesson-1/src/part1.js
@@ -0,0 +1,4 @@
+"use strict";
+let Tc = +prompt("input Celsius temperature");
+let Tf =  (9 / 5) * Tc + 32;
+alert(Tf);
---
+++ lesson-1/src/part2.js
@@ -0,0 +1,8 @@
+"use strict";
+let Admin;
+let Name;
+
+Name = "Василий";
+Admin = Name;
+
+console.log(Admin);
